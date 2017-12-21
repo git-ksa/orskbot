c25cfb80d4d1b21c11fd040b3e3e5ed553bf2428
Alexey Povazhniy <apovazhniy@rocketsoftware.com>
2017-12-21 14:26:51 +0500

    Command for Chelyabinsk

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import re
+import pytz
 import random
 import datetime
 import telebot
@@ -12,6 +13,81 @@ from datetime import time
 import config
 
 bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=['wc', 'пч'])
+@bot.message_handler(regexp="^.пч$")
+def send_weather(message):
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=1508291&units=metric&mode=xml&appid=7cad4e5a16fc989137d9dcaa7d726ff8')
+    if r.status_code == 200:
+        doc = xmltodict.parse(r.text)
+        temperature = doc['current']['temperature']['@value']
+    else:
+        temperature = 'хз сколько там за бортом, оденьте шарф'
+
+    tz = 'Asia/Yekaterinburg'
+    fmt = '%Y-%m-%d %H:%M:%S'
+    utc = datetime.datetime.utcnow()
+    now = timezone(tz).fromutc(utc)
+    time_str = timezone(tz).fromutc(utc).strftime(fmt)
+    now_time = now.time()
+    if now_time <= time(22,00) or now_time >= time(11,00):
+        station = '9900854'
+    else:
+        station = '9900846'
+
+    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0'}
+    s = requests.Session()
+    r = s.get('https://yandex.ru/maps/api/masstransit/getStopInfo', headers = user_agent)
+    r_dict = r.json()
+    token = r_dict.get('csrfToken')
+
+    url = 'https://yandex.ru/maps/api/masstransit/getStopInfo?csrfToken=' + token + '&host_config[hostname]=yandex.ru&id=stop__' + station + '&locale=ru_RU&mode=prognosis'
+    r = s.get(url, headers = user_agent)
+    r_dict = r.json()
+
+    try:
+        station_name = r_dict['data']['properties']['name']
+        current_time = r_dict['data']['properties']['currentTime']
+    except KeyError:
+        bot.send_message(message.chat.id, 'За бортом: ', temperature) 
+        bot.send_message(message.chat.id, 'C пепелацем произошла какаято хуйня...')
+        return
+
+    fmt = '%H:%M:%S'
+    local = pytz.timezone('Europe/Moscow')
+    naive = datetime.datetime.strptime(current_time, '%a %b %d %Y %H:%M:%S GMT+0300 (MSK)')
+    local_time = local.localize(naive, is_dst=None)
+    current_time = local_time.astimezone(pytz.timezone('Asia/Yekaterinburg'))
+
+    message_list = []
+    message_list.append('Галактика: ' + station_name)
+    message_list.append('Местное время: ' + current_time.strftime(fmt))
+    message_list.append('За бортом: ' + temperature)
+
+    try:
+        for item in r_dict['data']['properties']['StopMetaData']['Transport']:
+            if item['name'] == '22':
+                count = 0;
+                if 'Events' in item['BriefSchedule']:
+                    for i in item['BriefSchedule']['Events']:
+                        estimate_time = datetime.datetime.strptime(i['Estimated']['value'], '%Y-%m-%dT%H:%M:%S+05:00')
+                        local = pytz.timezone('Asia/Yekaterinburg')
+                        local_time = local.localize(estimate_time, is_dst=None)
+                        delta_time = local_time - current_time
+                        hours, remainder = divmod(delta_time.seconds, 3600)
+                        minutes, seconds = divmod(remainder, 60)
+                        if minutes > 30: continue
+                        count += 1
+                        message_list.append('Пепелац №22 прибывает через: ' + str(minutes) + ' мин. ' + str(seconds) + ' сек. на ' + str(count) + ' путь')
+                if count == 0: message_list.append('Пепелац №22 пока не прибывает, ждите')
+    except KeyError:
+        bot.send_message(message.chat.id, 'За бортом: ', temperature)
+        bot.send_message(message.chat.id, 'C пепелацем произошла какаято хуйня...')
+        return
+    msg = ''
+    for item in message_list:
+        msg += item + '\n'
+    bot.send_message(message.chat.id, msg)
 
 @bot.message_handler(commands=['w', 'п'])
 @bot.message_handler(regexp="^.п$")
